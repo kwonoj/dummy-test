5bbd952a11850317f9772ccc7bffba47219d7436
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-01-20 09:07:46 -0800

    build(cargo): init

---
+++ crates/bindings/src/lib.rs
@@ -0,0 +1,8 @@
+use next_binding::swc::core::{
+    base::config::ModuleConfig,
+
+};
+
+pub fn add() -> ModuleConfig {
+    ModuleConfig::Es6
+}
---
+++ crates/core/src/lib.rs
@@ -0,0 +1,7 @@
+use next_binding::swc::core::{
+    base::config::ModuleConfig,
+};
+
+pub fn add() -> ModuleConfig {
+    ModuleConfig::Es6
+}
